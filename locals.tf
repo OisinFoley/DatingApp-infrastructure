b0759146355a01c1f5642ba19990a6070fdfe5c5
OisinFoley <oisin.foley@jayway.com>
2022-12-05 22:44:20 +0100

    update locals

--- locals.tf
+++ locals.tf
@@ -17,7 +17,7 @@ locals {
     ticket_number  = "n/a"
   }
   resource_name_prefix          = "${var.business_division}-${var.environment}-${var.region_abbreviation}"
-  resource_name_prefix_dashless = "${var.business_division}${var.environment}${var.region_abbreviation}"
+  resource_name_prefix_dashless = replace(local.resource_name_prefix, "-", "")
   resource_group_name           = "${local.resource_name_prefix}-rg"
   virtual_network_name          = "${local.resource_name_prefix}-vnet"
   subnet_web_name               = "${var.web_subnet_name}-${local.resource_name_prefix}"
@@ -27,6 +27,6 @@ locals {
   web_linuxvm_nic_name          = "${local.resource_name_prefix}-${local.web_linuxvm_prefix}-nic"
   web_linuxvm_public_ip         = "${local.resource_name_prefix}-${local.web_linuxvm_prefix}-publicip"
   managed_identity_name         = "${local.resource_name_prefix}-user-assigned-mid"
-  key_vault_name                = "${local.resource_name_prefix_dashless}kv"
-  storage_account_name          = "${local.resource_name_prefix_dashless}str"
+  key_vault_name                = "${local.resource_name_prefix_dashless}kvtmp"
+  storage_account_name          = "${local.resource_name_prefix_dashless}strtmp"
 }
